95ee5cb3f5ade7e7c21ae275717cc8c0b297c578
Shinigami <102893295+shadowlight671@users.noreply.github.com>
2023-10-12 16:49:22 +0600

    Add toast message on button

--- app/src/main/java/com/example/basics/MainActivity.kt
+++ app/src/main/java/com/example/basics/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.basics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +16,12 @@ class MainActivity : AppCompatActivity() {
         val et : TextView = findViewById(R.id.editText1)
 
         val editTextInput = et.text
+
+        // Button view
+        val btn : Button = findViewById(R.id.button)
+        // Handle event click on button
+        btn.setOnClickListener(){
+            Toast.makeText(this@MainActivity, "Button has been clicked", Toast.LENGTH_LONG).show()
+        }
     }
 }
