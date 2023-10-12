d7733bfb6f1a57aca1b4d044443661b9832b9bcf
Shinigami <102893295+shadowlight671@users.noreply.github.com>
2023-10-12 16:37:06 +0600

    Functionality for EditText view

--- app/src/main/java/com/example/basics/MainActivity.kt
+++ app/src/main/java/com/example/basics/MainActivity.kt
@@ -3,12 +3,16 @@ package com.example.basics
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // EditText View
+        val et : TextView = findViewById(R.id.editText1)
 
+        val editTextInput = et.text
     }
 }
